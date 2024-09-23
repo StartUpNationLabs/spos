253690e36f8f79da936f91cc7f27f82b4262f087
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2024-09-23 20:06:30 +0200

    test: fix tests

---
+++ apps/sample-react/src/app/app.spec.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import App from './app';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+describe('App', () => {
+  it('should render successfully', () => {
+    const queryClient = new QueryClient()
+    const { baseElement } = render(<QueryClientProvider client={queryClient}><App /></QueryClientProvider>);
+    expect(baseElement).toBeTruthy();
+  });
+});
