57ed740afab7be764906864dfab6784230ef9ac9
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2024-09-23 08:36:02 +0200

    fix: remove the possiblilty to create emtpy groups

--- libs/ui/common/src/lib/tables/landingPage.tsx
+++ libs/ui/common/src/lib/tables/landingPage.tsx
@@ -2,13 +2,15 @@ import {useNavigate} from "react-router-dom";
 import {Box, Button, Typography} from "@mui/material";
 import * as React from 'react';
 import {TableGrid} from "./tableGrid";
+import {useCurrentSelectedGroup} from "./stores/currentSelectedGroup";
 
 export function LandingPage() {
   const navigate = useNavigate();
   const navigateTables = () => {
     navigate("/offers")
   }
-
+  const tables  = useCurrentSelectedGroup(state => state.tables);
+  const tableEmpty = Object.keys(tables).length === 0;
   return (
     <Box height={"100vh"} width={"100%"} display={"flex"} alignItems={"center"} flexDirection={"column"} justifyContent={"center"}>
       <Typography align='center' variant="h1" component="h2" fontSize="10vh" fontWeight="bold">
@@ -20,6 +22,7 @@ export function LandingPage() {
           onClick={navigateTables}
           variant="contained"
           color="success"
+          disabled={tableEmpty}
         >
           <Typography variant={"h4"}>Create</Typography>
         </Button>
