c04d91309c5776970e5c62727b99a245dc45bf7e
Tsukoyachi <Axel.delille31@gmail.com>
2024-09-29 15:59:21 +0200

    fix: Clean logs from orders

--- libs/ui/common/src/lib/orders/section.tsx
+++ libs/ui/common/src/lib/orders/section.tsx
@@ -9,34 +9,31 @@ type SectionProps = {
     onSelectOrder: any
 }
 
-const Section = ({ title, orders, selectedOrder, onSelectOrder }: SectionProps) => {
-    console.table(orders);
-    return (
-        <Box sx={{ marginBottom: 3 }}>
-            <Typography variant="h6" gutterBottom sx={{ color: 'black', fontSize: 24 }}>
-                {title}
-            </Typography>
-            {Object.entries(orders).map(([table, ordersList], idx) => (
-                <Box key={idx} sx={{ marginBottom: 2 }}>
-                    <Typography variant="subtitle1" sx={{ color: 'black', fontSize: 20 }}>
-                        {`Table ${table} :`}
-                    </Typography>
-                    <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
-                        {ordersList.map((order, index) => (
-                            <CommandNumber
-                                key={index + 1}
-                                number={index + 1} // TODO: Change By a unique number
-                                status={order.status}
-                                isServed={false} //TODO: Need input from backend
-                                isSelected={selectedOrder && selectedOrder.preparationId === order.preparationId}  // Comparer pour savoir si c'est sélectionné
-                                onSelect={() => onSelectOrder(title.toLowerCase(), table, order.preparationId)}  // Passer la commande sélectionnée
-                            />
-                        ))}
-                    </Box>
+const Section = ({ title, orders, selectedOrder, onSelectOrder }: SectionProps) => (
+    <Box sx={{ marginBottom: 3 }}>
+        <Typography variant="h6" gutterBottom sx={{ color: 'black', fontSize: 24 }}>
+            {title}
+        </Typography>
+        {Object.entries(orders).map(([table, ordersList], idx) => (
+            <Box key={idx} sx={{ marginBottom: 2 }}>
+                <Typography variant="subtitle1" sx={{ color: 'black', fontSize: 20 }}>
+                    {`Table ${table} :`}
+                </Typography>
+                <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
+                    {ordersList.map((order, index) => (
+                        <CommandNumber
+                            key={index + 1}
+                            number={index + 1} // TODO: Change By a unique number
+                            status={order.status}
+                            isServed={false} //TODO: Need input from backend
+                            isSelected={selectedOrder && selectedOrder.preparationId === order.preparationId}  // Comparer pour savoir si c'est sélectionné
+                            onSelect={() => onSelectOrder(title.toLowerCase(), table, order.preparationId)}  // Passer la commande sélectionnée
+                        />
+                    ))}
                 </Box>
-            ))}
-        </Box>
-    )
-};
+            </Box>
+        ))}
+    </Box>
+);
 
 export default Section;
