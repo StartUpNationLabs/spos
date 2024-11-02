7df3d5aba1d8c9e7b552821d54c4348d0d17ccea
Anna <annadiplacido3@gmail.com>
2024-11-02 22:19:10 +0100

    feat : add return navigate for diningRoomTables

--- libs/ui/common/src/lib/dining/diningRoomTables.tsx
+++ libs/ui/common/src/lib/dining/diningRoomTables.tsx
@@ -2,12 +2,21 @@ import React, { useState } from 'react';
 import './diningRoomTables.css';
 import DiningRoomSVG from './diningRoomSvg';
 import DiningHeader from './diningHeader';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export function DiningRoomTables() {
+  const navigate = useNavigate();
+  const { groupId, tableNumber } = useParams<{ 
+    groupId: string; 
+    tableNumber: string;
+  }>();
+
   const [hasSelection, setHasSelection] = useState(false);
 
   const handleBackClick = () => {
     console.log('Back button clicked');
+    navigate(`/mealSelectionForPayment/${groupId}/${tableNumber}`);
+
   };
 
   const handleContinueClick = () => {
